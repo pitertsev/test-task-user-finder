9da21a5c79e03daa97aadfd9af6f2b7e849767c3
VanMexico <ivan@pitertsev.pw>
2020-11-20 15:00:36 +0300

    Added title component

---
+++ src/components/Title.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1 v-if="tag === 'h1'" :class="classes">{{ title }}</h1>
+  <h2 v-else-if="tag === 'h2'" :class="classes">{{ title }}</h2>
+  <h3 v-else-if="tag === 'h3'" :class="classes">{{ title }}</h3>
+  <h4 v-else-if="tag === 'h3'" :class="classes">{{ title }}</h4>
+  <h5 v-else-if="tag === 'h3'" :class="classes">{{ title }}</h5>
+  <h6 v-else-if="tag === 'h3'" :class="classes">{{ title }}</h6>
+</template>
+
+<script>
+export default {
+  name: 'Title',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    tag: {
+      type: String,
+      default: 'h1'
+    },
+    classes: {
+      type: String,
+    }
+  }
+}
+</script>
+
--- src/views/User.vue
+++ src/views/User.vue
@@ -2,7 +2,7 @@
   <b-container>
     <b-row>
       <b-col cols="10" offset="1">
-        <h1>Пользователь: {{ user.login }}</h1>
+        <Title :title="titleGenerate" />
         <UserInfo :user="user"/>
       </b-col>
     </b-row>
@@ -11,9 +11,10 @@
 
 <script>
 import UserInfo from '../components/UserInfo.vue'
+import Title from '../components/Title.vue'
 export default {
   name: "User",
-  components: {UserInfo},
+  components: {Title, UserInfo},
   data: () => ({
     user: null
   }),
@@ -21,5 +22,10 @@ export default {
     const id = this.$route.params.id
     this.user = this.$store.getters.getUser(id)
   },
+  computed: {
+    titleGenerate() {
+      return `Пользователь: ${this.user.login}`
+    }
+  }
 }
 </script>
